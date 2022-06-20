0f3ab5a381bcfde3e0854d5940c60f3ef5460068
Oleks Kryo <amzkryvchenko@gmail.com>
2022-06-20 12:43:42 -0300

    object.values frunction and items to shoparray added

--- src/pages/Shop/SingleItem.js
+++ src/pages/Shop/SingleItem.js
@@ -6,11 +6,8 @@ import { Paper } from "@mui/material";
 export const SingleItem = () => {
   const { name } = useParams();
   const thisArray = shopArray.find((prod) => prod.name === name);
-  // const [imageColor, setImageColor] = useState(thisArray.images);
-
-  // function toggleColor() {
-  //   setImageColor();
-  // }
+  const colors = thisArray.images;
+  const [imageColor, setImageColor] = useState({});
 
   return (
     <section className="single-item">
@@ -22,10 +19,23 @@ export const SingleItem = () => {
           <h1 className="description-title">{thisArray.description}</h1>
           <div className="fulldescription">{thisArray.fulldescription}</div>
           <h3 className="description-title">Price: ${thisArray.price}</h3>
-          {/* <button onClick={toggleColor}>Toggle color</button> */}
+          <button>Math Black</button>
+          {/* <button onClick={() => setImageColor(thisArray.images.color3)}>
+            Math Black
+          </button>
+          <button onClick={() => setImageColor(thisArray.images.color1)}>
+            Math Black
+          </button> */}
         </div>
         <div className="item-content-left">
-          <img src={thisArray.image} alt="" className="single-item-img" />
+          {Object.values(colors).map((value, index) => {
+            return (
+              <div key={index}>
+                <img src={value} alt="" className="single-item-img " />
+                <hr />
+              </div>
+            );
+          })}
         </div>
       </Paper>
       <Link to="/shop" className="back-link">
--- src/pages/Shop/shopArray.js
+++ src/pages/Shop/shopArray.js
@@ -1,27 +1,62 @@
 export const shopArray = [
   {
     id: 1,
-    name: "Nolan N100",
-    description: "This is Nolan N100 Pro",
-    fulldescription: "",
+    name: "Nolan N70-2 GT",
+    description: "HELMET NOLAN N70-2 GT N-COM",
+    fulldescription:
+      "Nolan N70-2 GT Spinnaker N-Com Helmet. The new N70-2 GT is the evolution of the successful Crossover Nolan Touring model, the N44 Evo. This is a fully modular helmet, which responds to the needs of the most experienced bikers as it allows up to 8 different configurations. The helmet has P/J homologation for riding with full safety with the helmet closed, in full face mode as well as with the helmet open in jet mode.",
     type: "helmet",
-    price: 2000,
-    image: "/images/demon-150-gr.jpeg",
-    category: "bikes",
+    features: {
+      feature1:
+        "Outer shell made of Lexan®TM polycarbonate, a high performance resin capable of withstanding strong impacts. ",
+      feature2: "2 shell sizes for a better fit to each size.",
+      feature3:
+        "The ventilation system has an inlet on chin guard and an Air Booster in the upper area which directs the airflow to the more important areas. It has an extractor on the rear to extract the hot air from inside the helmet. ",
+      feature4:
+        "Removable chin curtain as standard. Isolates the chin from direct air. ",
+      feature5:
+        "Ultrawide clear screen which offers a wide field of vision. With anti-scratch treatment. In jet configuration, its wide screen offers great protection against wind and rain",
+      feature6:
+        "Includes Pinlock to prevent fogging of the screen. In addition, the screen incorporates a system on the outside that allows you to adjust the Pinlock without the need to remove it.",
+    },
+    price: 399,
+    image: "/images/nolan/n70-2-gt-lakota.webp",
+    images: {
+      color1: "/images/nolan/n70-2-gt-lakota.webp",
+      color2: "/images/nolan/n70-2-gt-metal-white.webp",
+      color3: "/images/nolan/n70-gt-black-yellow.webp",
+    },
+    category: "helmets",
     all: "all",
   },
   {
     id: 2,
     name: "Nolan N33",
-    description: "This is Nolan N40 Pro",
+    description: "HELMET NOLAN N33 EVO",
     fulldescription:
-      "Nolan N33 Evo Classic Helmet Slate Grey. The N33 Evo is a demi-jet type helmet made of high resistance polycarbonate. It stands out for its wide panoramic screen that offers excellent visibility and that together with the built-in sun visor is ideal for daily use and intercity trips. It has a ventilation system with air inlets and extractors for optimal aeration. The interior padding is fully removable and washable.",
-    type: "bike",
-    price: 1850,
+      "Nolan N33 Evo Classic Helmet. The N33 Evo is a demi-jet type helmet made of high resistance polycarbonate. It stands out for its wide panoramic screen that offers excellent visibility and that together with the built-in sun visor is ideal for daily use and intercity trips. It has a ventilation system with air inlets and extractors for optimal aeration. The interior padding is fully removable and washable.",
+    type: "helmet",
+    features: {
+      feature1:
+        "Outer shell made of polycarbonate with high performance against impacts.",
+      feature2: "EPS inner shell in 4 sizes for a better fit in each size.",
+      feature3:
+        "Clear Ultrawide display, offering a wide field of view. It has anti-scratch treatment.",
+      feature4:
+        "Inner VPS solar visor made of Lexan™ with anti-scratch and anti-fog treatment, with UV protection up to 400 nanometers. It is easily removable (without the need for tools) and can be operated from the outside.",
+      feature5:
+        "The ventilation system consists of an air inlet on the screen and an Air Booster in the upper area that directs the air to the most important areas. It has an air extractor on the back to extract hot air from inside the helmet.",
+      feature6:
+        "Fully removable and washable Clima Confort interior padding made of double density micro-perforated fabric. This fabric offers great comfort under all weather conditions.",
+    },
+    price: 329,
     image: "/images/nolan/n33-classic-black.webp",
     images: {
-      image1: "/images/nolan/n33-classic-black.webp",
-      image2: "/images/nolan/n33-matt-black.webp",
+      color1: "/images/nolan/n33-classic-black.webp",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/n33-vulcan-grey.webp",
     },
     category: "helmets",
     all: "all",
@@ -29,23 +64,57 @@ export const shopArray = [
   {
     id: 3,
     name: "Nolan N21",
-    description: "This is Nolan N21 Pro",
-    fulldescription: "",
+    description: "HELMET NOLAN N21 VISOR ",
+    fulldescription:
+      "Nolan N21 Visor Classic Helmet. Of 'vintage' style, the N21 Visor is the result of adding a wide profiled screen to the N21 that covers a large area, thus making it a helmet suitable for urban as well as extra-urban use. It also has a dark VPS solar visor, which can be easily operated from outside.",
     type: "helmet",
-    price: 1600,
-    image: "/images/demon-150-gr.jpeg",
-    category: "bikes",
+    features: {
+      feature1: "Outer shell in polycarbonate.",
+      feature2: "2 shell sizes for a better fit to each size.",
+      feature3: "Wide clear anti-scratch screen.",
+      feature4: "Dark solar visor (VPS) operable from outside.",
+      feature5:
+        "Removable and washable inner padding, in Clima Comfort fabric.",
+      feature6: "Ring for locking the helmet to the bike (Helmet Lock Ring)",
+    },
+    price: 299,
+    image: "/images/nolan/n21-classic-black.webp",
+    images: {
+      color1: "/images/nolan/n21-classic-black.webp",
+      color2: "/images/nolan/n21-getaway.webp",
+      color3: "/images/nolan/n21-quarterback-f-black.webp",
+    },
+    category: "helmets",
     all: "all",
   },
   {
     id: 4,
-    name: "Nolan N70",
-    description: "This is Nolan N33",
-    fulldescription: "",
-    type: "bike",
-    price: 1400,
-    image: "/images/n-100.jpeg",
-    category: "helmets",
+    name: "Nolan N40",
+    description: "HELMET NOLAN N40-5 BELTWAY N-COM",
+    fulldescription:
+      "Nolan N40-5 Beltway N-Com Helmet. Urban jet-type helmet made of high-resistance polycarbonate. It stands out for the wide adjustable panoramic screen with anti-scratch treatment that protects from wind and light rain. It incorporates the sun visor (VPS) against easily operable UVA / UVB rays. The rear extractor ventilation system provides optimal air circulation. Ideal for urban and interurban use. It is predisposed for the Nolan “N-Com” intercom system. (not included)",
+    type: "helmet",
+    features: {
+      feature1:
+        "Outer shell made of polycarbonate with high performance against strong impacts.",
+      feature2: "EPS inner shell in 4 sizes for a better fit in each size.",
+      feature3:
+        "It stands out for offering up to 8 possible configurations for riding.",
+      feature4:
+        "Inner VPS sun visor with anti-scratch and anti-fog treatment, with UV protection up to 400 nanometers. It is easily removable (without the need for tools) and can be operated from the outside.",
+      feature5:
+        "The ventilation system consists of an air inlet on the screen and an Air Booster in the upper area that directs the air to the most important areas. It has an air extractor on the back to extract hot air from inside the helmet.",
+      feature6:
+        "Microlock micrometric closure. The straps are padded for comfort.",
+    },
+    price: 329,
+    image: "/images/nolan/n40-beltway.webp",
+    images: {
+      color1: "/images/nolan/n40-beltway.webp",
+      color2: "/images/nolan/n40-salt-silver.webp",
+      color3: "/images/nolan/n40-w-b.webp",
+    },
+    category: "bikes",
     all: "all",
   },
   {
@@ -56,6 +125,13 @@ export const shopArray = [
     type: "helmet",
     price: 1200,
     image: "/images/n-100.jpeg",
+    images: {
+      color1: "/images/n-100.jpeg",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/vulkan-grey.webp",
+    },
     category: "helmets",
     all: "all",
   },
@@ -67,6 +143,13 @@ export const shopArray = [
     type: "bike",
     price: 1000,
     image: "/images/n-100.jpeg",
+    images: {
+      color1: "/images/n-100.jpeg",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/vulkan-grey.webp",
+    },
     category: "helmets",
     all: "all",
   },
@@ -78,6 +161,13 @@ export const shopArray = [
     type: "helmet",
     price: 1000,
     image: "/images/n-100.jpeg",
+    images: {
+      color1: "/images/n-100.jpeg",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/vulkan-grey.webp",
+    },
     category: "helmets",
     all: "all",
   },
@@ -89,6 +179,13 @@ export const shopArray = [
     type: "bike",
     price: 1000,
     image: "/images/n-100.jpeg",
+    images: {
+      color1: "/images/n-100.jpegs",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/vulkan-grey.webp",
+    },
     category: "helmets",
     all: "all",
   },
@@ -100,6 +197,13 @@ export const shopArray = [
     type: "helmet",
     price: 1000,
     image: "/images/n-100.jpeg",
+    images: {
+      color1: "/images/n-100.jpeg",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/m33-vulkan-grey.webp",
+    },
     category: "helmets",
     all: "all",
   },
@@ -111,6 +215,13 @@ export const shopArray = [
     type: "phone",
     price: 1000,
     image: "/images/n-100.jpeg",
+    images: {
+      color1: "/images/n-100.jpeg",
+      color2: "/images/nolan/n33-matt-black.webp",
+      color3: "/images/nolan/n33-slat-gray.webp",
+      color4: "/images/nolan/n33-white.webp",
+      color5: "/images/nolan/vulkan-grey.webp",
+    },
     category: "helmets",
     all: "all",
   },

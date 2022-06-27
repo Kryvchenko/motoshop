f48f666fec5ef9bd2c6833414435e04777f20fba
Oleks Kryo <amzkryvchenko@gmail.com>
2022-06-27 10:16:49 -0300

    typewriter added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,14 +7,40 @@ import { About } from "../../components/About/About";
 import { Features } from "../../components/Features/Features";
 import Maphome from "../../components/Map/Maphome";
 import { Form } from "../../components/Form/Form";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 export const Home = () => {
+  const { text } = useTypewriter({
+    words: ["A BIKE?", "SERVICE? ", "A HELMET?", "SPARE PARTS?"],
+    loop: 0,
+  });
+
   return (
     <>
       <section className="hero-section">
         <video src="/videos/front-street.mp4" autoPlay loop muted />
         <div className="hero-content">
           {" "}
+          <center>
+            <h1
+              className="hero-title"
+              style={{
+                color: "#ff4b52",
+                fontWeight: "bolder",
+              }}
+            >
+              NEED{" "}
+              <span
+                style={{
+                  color: "white",
+                  fontWeight: "bolder",
+                }}
+              >
+                {text}
+                <Cursor />
+              </span>
+            </h1>
+          </center>
           <NavLink to="/shop" rel="noopener">
             <button className="hero-btn">SHOP NOW</button>
           </NavLink>

a25ea5a12c067ecfc979ea2d9bb650cd5da7fed2
Oleks Kryo <amzkryvchenko@gmail.com>
2022-06-29 16:29:30 -0300

    bug fixed

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Form } from "../../components/Form/Form";
 import { AboutPage } from "../../pages/About/AboutPage";
--- src/index.js
+++ src/index.js
@@ -4,22 +4,13 @@ import "./index.css";
 import "./grid.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
-// import { configureStore } from "@reduxjs/toolkit";
-import { productsLikeReducer } from "./Redux/productsLikeReducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const store = createStore(
-  productsLikeReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   // </React.StrictMode>
 );
